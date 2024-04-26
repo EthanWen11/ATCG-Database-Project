8aef94de3a6dab02b17fc3a0ea3edb8c6fc49aab
Kyle Chen <chenchhaykyle@gmail.com>
2024-04-26 00:12:47 -0700

    more basic file structure setup

---
+++ atcg/src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// will accept prop with all the card data included for it to render
+const Card = () => {
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Card
