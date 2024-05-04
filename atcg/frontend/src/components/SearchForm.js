b98dd1f10a1293dfc4281fdc2145f22e360f29d5
Kyle Chen <kyle.chen@sjsu.edu>
2024-05-03 18:49:47 -0700

    edit

--- atcg/frontend/src/components/SearchForm.js
+++ atcg/frontend/src/components/SearchForm.js
@@ -7,8 +7,9 @@ const SearchForm = () => {
     <>
       <div className='p-0 flex flex-col flex-nowrap'>
         <form className='mt-[30] mx-auto max-w-lg relative w-full' action="" acceptCharset='UTF-8' method='get'>
-          <a className='block absolute z-[500] w-20 h-full p-4 left-0' >
+          <a className='block absolute z-[500] w-20  left-4 top-1/4' >
             <span className='border-0 h-1 -m-1 overflow-hidden p-0 absolute w-1'>TCG name</span>
+            {/* placeholder svg */}
             <Logo className='text-purple-700' width='48px' height='48px' />
           </a>
           <input className='border-solid border-2 border-white bg-slate-700 text-white py-8 pr-14 pl-20 text-base rounded-md shadow-sm m-0 w-full' type="text" name='query' id='query' autoCapitalize='none' autoCorrect='off' spellCheck='false' maxLength='1024' />
