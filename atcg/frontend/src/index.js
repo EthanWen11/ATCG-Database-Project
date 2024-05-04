eeeb91d5667f24677f1fbbf418765d2b643cd4b1
Matt E <mathew.estrada@sjsu.edu>
2024-05-03 18:35:19 -0700

    new frontend branch

--- atcg/frontend/src/index.js
+++ atcg/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home.tsx'
+import Home from './pages/Home.js'
 import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
