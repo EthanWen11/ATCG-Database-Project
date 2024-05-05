7ce21c672339f70e7eed79acb190e9c24e8e1783
Matt E <mathew.estrada@sjsu.edu>
2024-05-05 13:57:59 -0700

    card animation update - homepage

--- atcg/frontend/src/pages/Search.js
+++ atcg/frontend/src/pages/Search.js
@@ -5,10 +5,16 @@ const Search = () => {
     const [searchParams] = useSearchParams();
     const query = searchParams.get('query');
 
+    
+    const textStyle = {
+        color: 'white',
+        marginTop: '100px', 
+    };
+
     // atm only displays the search query
     return (
-        <div>
-            {query}
+        <div className="min-h-screen bg-gradient-to-b from-gray-600 to-indigo-900 flex flex-col items-center">
+            <span style={textStyle}>{query}</span>
         </div>
     )
 }
