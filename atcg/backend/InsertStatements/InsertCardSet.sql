d8ccd7e27b81679f11d27b79c81776a2cdd6184e
Kyaw Soe Han <42433674+meteor123456@users.noreply.github.com>
2024-05-08 21:29:41 -0700

    Added Insert Statements

---
+++ atcg/backend/InsertStatements/InsertCardSet.sql
@@ -0,0 +1,13 @@
+INSERT INTO CardSet (setID,setName,designer,logo)
+VALUES
+('COR','Core Set','Shub Takkorath','logo-core'),
+('HOE','Hell on Earth','Aedriaus Olive',''),
+('ELM','Elementary','Elaina Synranelt',''),
+('WHV','Shadows of Waterhaven','Shub Takkorath','logo-whv'),
+('DEP','Depths of Triton','Shub Takkorath','logo-dep'),
+('QIL','The Qil''kuoth War','Shub Takkorath','logo-qil'),
+('CLA','Mizuki & Caerula Arbor','Shub Takkorath','logo-cla'),
+('KGD','Kyros'' Guide to Dragons','Shub Takkorath','logo-kgd'),
+('NIN','Murder on Nexus Nine','Shub Takkorath','logo-nin'),
+('JGK','Escape from Jigoku','Shub Takkorath','logo-jgk'),
+('FAL','Starfall','Shub Takkorath','logo-fal');
