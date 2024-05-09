bca886d57cb8dc7a913addc935fefff3136633b8
Kyle Chen <chenchhaykyle@gmail.com>
2024-05-08 19:53:37 -0700

    changed the db config back to normal

--- atcg/backend/database.js
+++ atcg/backend/database.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const dbConfig = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'pass123',
+    password: process.env.DB_PASSWORD || '123456',
     database: process.env.DB_NAME || 'atcg'
 };
 
